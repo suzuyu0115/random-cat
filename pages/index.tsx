bfef5c182f491f5bfeca537d29dea870faa74dbc
suzuyu0115 <szkyut0115@gmail.com>
2024-05-12 12:59:17 +0900

    データフェッチAPIを使ってリクエスト時に初期画像を取得する

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,46 @@
-import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
-const IndexPage: NextPage = () => {
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
+type Props = {
+  initialImageUrl: string;
+};
 
-  useEffect(() => {
-    fetchImage().then((newImage) => {
-      setImageUrl(newImage.url);
-      setLoading(false);
-    })
-  }, []);
-
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url);
+  //     setLoading(false);
+  //   });
+  // }, []);
   const handleClick = async () => {
     setLoading(true);
     const newImage = await fetchImage();
     setImageUrl(newImage.url);
     setLoading(false);
   };
-
   return (
     <div>
       <button onClick={handleClick}>他のにゃんこも見る</button>
       <div>{loading || <img src={imageUrl} />}</div>
     </div>
   );
-}
+};
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
 
 type Image = {
   url: string;
 };
-
 const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
